d40bcba3f2cb9d818d63ff6aacef6848ebf9e911
Vishesh Bardia <developvishesh@gmail.com>
2022-02-26 12:20:18 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculate(){
+    let height = document.getElementById('height').value;
+    height /=100;
+    let weight = document.getElementById('weight').value;
+    let bmi = weight/(height*height);
+    
+    document.getElementById('bmi').innerHTML = '<strong>' + bmi.toFixed(2) + '</strong>';
+}
